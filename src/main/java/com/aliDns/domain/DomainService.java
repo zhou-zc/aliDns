ff56679724e3ee6478e91f90c342f71d0c0c8a47
zhou-zc <zc.zhou.0234@gmail.com>
2024-06-13 10:39:01 +0800

    # :sparkles: 添加测试名称

--- src/main/java/com/aliDns/domain/DomainService.java
+++ src/main/java/com/aliDns/domain/DomainService.java
@@ -1,5 +1,6 @@
 package com.aliDns.domain;
 
+import com.github.promeg.pinyinhelper.Pinyin;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,5 +12,7 @@ public class DomainService {
      * 添加域名
      */
     public void addDomain(){
+        String 中文域名 = Pinyin.toPinyin("中文域名", "");
+        System.out.println("中文域名 = " + 中文域名);
     }
 }
